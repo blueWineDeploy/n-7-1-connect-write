393aecca357456186d1cd434d9fe4ccdbba84353
kenny <ihomerun21@msn.com>
2017-04-21 11:22:47 -0700

    Modified mongoDB connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect('mongodb://getitdone:120606@ds031867.mlab.com:31867/todoapp' || 'mongodb://localhost:3000/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://getitdone:120606@ds031867.mlab.com:31867/todoapp');
 
 module.exports = {mongoose};
 
